cf4d8263ed37ce7bc616da57690c03814db33754
lianfeiru <13932455231@qq.com>
2025-03-27 03:02:53 +0800

    修改样式

--- docs/.vitepress/theme/components/Features.vue
+++ docs/.vitepress/theme/components/Features.vue
@@ -34,7 +34,7 @@
   .features {
     display: grid;
     /* grid-template-columns: repeat(3, 1fr); */
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     grid-gap:20px;
   }
   
